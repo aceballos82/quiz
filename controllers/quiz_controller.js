be06822dada9c3018d1e4d2ef1d1e58542c8876d
Adriana Ceballos <a.ceballos82@gmail.com>
2015-06-08 12:10:42 +0200

    Practica Obligatoria Modulo 6

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,7 +11,9 @@ exports.answer = function(req,res){
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
 	}
 };
-exports.question = function(req,res){
+exports.author = function(req,res){
 	res.render('author', {});
 };
+
+
 
